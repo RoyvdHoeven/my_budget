0b521217db33498541931194d2c499edf333f83c
[Roy van der Hoeven] <[royvanderhoeven2208@gmail.com]>
2025-04-29 21:35:07 +0200

    setup

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(ProviderScope(child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text('MyBudget', style: TextStyle(color: Colors.white),),
+        ),
+        body: Container(
+          child: const Text('data'),
+        ),
+      ),
+    );
+  }
+}
+
